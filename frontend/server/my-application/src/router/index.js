29b367f81b0f05fee681045df53492f2df3bb799
CowardHedgehog <134586429+CowardHedgehog@users.noreply.github.com>
2025-05-19 17:22:05 +0900

    routerの修正

--- frontend/server/my-application/src/router/index.js
+++ frontend/server/my-application/src/router/index.js
@@ -2,9 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { c_root } from './common_router'
 import { s_root } from './student_router'
 import { t_root } from './teacher_router'
-import { test_root } from './test_router.js'
+// import { test_root } from './test_router.js'
 
-const routes = [...c_root, ...s_root, ...t_root, ...test_root]
+// const routes = [...c_root, ...s_root, ...t_root, ...test_root]
+const routes = [...c_root, ...s_root, ...t_root]
 
 const router = createRouter({
   history: createWebHistory(),
